97269daf2cea18874ae1406fafefff15be75fea6
Joost van Wollingen <jvanwollingen@bol.com>
2017-02-10 13:17:11 +0100

    - Updated images - Renamed images to webserver and database

--- docker-images/webserver/app.py
+++ docker-images/webserver/app.py
@@ -1,5 +1,6 @@
 import os
 import pgdb
+import logging
 
 from flask import Flask
 
@@ -7,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    conn = pgdb.connect( host='pg', user='postgres', password='testnet', database='testnet' )
+    conn = pgdb.connect( host='database', user='postgres', password='testnet', database='testnet' )
     cur = conn.cursor()
     cur.execute( "SELECT value FROM kv WHERE key='provider'" )
     provider = cur.fetchone()[0]
@@ -15,6 +16,7 @@ def hello():
     return 'Hello '+provider+'!'
 
 if __name__ == '__main__':
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
